7f00cc775c7a46f98a0195816aad3dd4525cdfec
Gabriel Tanod <gabrielctanod@gmail.com>
2026-02-23 14:09:26 +0700

    Initial Commit

---
+++ Insomnio/InsomnioApp.swift
@@ -0,0 +1,32 @@
+//
+//  InsomnioApp.swift
+//  Insomnio
+//
+//  Created by Gabriel Tanod on 23/02/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct InsomnioApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
